09e8fab2d8a8ea130dd76d70c1417b133fb9d9d3
Ghanta Sai Krishna <84200397+sai-krishna-ghanta@users.noreply.github.com>
2021-08-06 16:36:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
+# adding lib symlink
+ln -s $BUILD_DIR/vendor/libreoffice/deps/lib/libGL.so.8.0.0 
+$BUILD_DIR/vendor/libreoffice/deps/lib/libGL.so.1
+
 echo "\
 [server]\n\
 headless = true\n\
