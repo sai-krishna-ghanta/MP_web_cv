1144b96f8d890300db87b0a204c4528fe8ef5d75
Ghanta Sai Krishna <84200397+sai-krishna-ghanta@users.noreply.github.com>
2021-08-06 20:46:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 from multiapp import MultiApp 
-from pages import face_detection_image,facemesh,hands,hostilic,objectron,pose,selfie_segmentation
+from pages import face_detection_image,facemesh,hands,hostilic,objectron,pose,selfie_segmentation,home
 import warnings
 warnings.filterwarnings("ignore")
-
+st.markdown("Tool Bar")
 app = MultiApp()
-st.text("Navigate")
+app.add_app("Home",home.app)
 app.add_app("Face detection", face_detection_image.app)
 app.add_app("Face Mesh", facemesh.app)
 app.add_app("Hands", hands.app)
 app.add_app("Hostilic", hostilic.app)
 app.add_app("Pose ", pose.app)
 app.add_app("selfie_segmentation", selfie_segmentation.app)
-
-st.text("Made by G Sai Krishna")
+app.add_app("3D Objectron",objectron.app)
 app.run()
